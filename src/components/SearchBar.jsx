912c9e44a3dff4b27c421fc4a8a399ed66d8c6fa
Aman Dangol <amandangol423@gmail.com>
2024-09-01 14:05:01 +0545

    made product container

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-edf
+import logo from "../assets/logo.png";
+import Profile from "./Profile";
+export default function SearchBar() {
+  return (
+    <div className="flex justify-between items-center text-md p-2">
+      <img src={logo} className="object-cover w-14  " alt="" />
+      <input
+        className="outline-none p-2 text-left w-[30%] bg-slate-200 rounded-xl"
+        type="text"
+        placeholder="Search"
+      />
+      <Profile />
+    </div>
+  );
+}

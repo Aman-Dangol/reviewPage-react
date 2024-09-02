8768ea51cec1ca09dcc645ee3d2048e82f3b376c
Aman Dangol <amandangol423@gmail.com>
2024-09-02 15:48:33 +0545

    mean stars added

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -3,7 +3,11 @@ import Stars from "./Stars";
 import ColorSelector from "./ColorSelector";
 import SizeSelector from "./SizeSelector";
 import { Heart } from "lucide-react";
+import { REVIEWS } from "../constants/Reviews";
 export default function ProductDetail() {
+  const avg = (
+    REVIEWS.reduce((acc, sum) => acc + parseInt(sum.stars), 0) / REVIEWS.length
+  ).toFixed(1);
   return (
     <section>
       <div className="flex justify-between p-2 ">
@@ -11,7 +15,7 @@ export default function ProductDetail() {
         <p className="text-slate-300">HR1235-800</p>
       </div>
       <h3 className="font-bold ">Shoes Reebok Zig Kinetica 3</h3>
-      <Stars number="4" width="20" height="20" />
+      <Stars number={avg} width="20" height="20" />
       <h1 className="font-bold py-3 mb-3 ">$199.00</h1>
       <ColorSelector />
       <SizeSelector />
